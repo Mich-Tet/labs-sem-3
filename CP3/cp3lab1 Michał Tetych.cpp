42a9c668ccf8d93aa11bed9995e92ff7a9f80738
Michal Tetych <michtet520@student.polsl.pl>
2021-10-28 12:42:42 +0200

    CP3

---
+++ CP3/cp3lab1 Michał Tetych.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+template<class T>
+class Myimg
+{
+	int arraySize;
+	T** myArray;
+public:
+	Myimg(int arraySize) ;
+
+	int getSize();
+	void assignValue(int x, int y, int arraySize);
+	
+	T returnValue(int x, int y);
+};
+template<class T>
+Myimg<T>::Myimg(int size)
+{
+	arraySize = size;
+	myArray = new T* [arraySize];
+	for (int i = 0; i < arraySize; i++)
+	{
+		myArray[i] = new T[arraySize];
+	}
+}
+template<class T>
+int Myimg<T>::getSize()
+{
+	return arraySize;
+}
+template<class T>
+void Myimg<T>::assignValue(int x,int y,int arraySize)
+{
+	myArray[x][y] = arraySize;
+}
+template<class T>
+T Myimg<T>::returnValue(int x, int y)
+{
+	return myArray[x][y];
+}
+int main()
+{
+	Myimg<double> a(100);
+	a.assignValue(5,10,175);
+	cout << a.getSize() << endl;
+	cout << a.returnValue(5, 10) << endl;
+}
